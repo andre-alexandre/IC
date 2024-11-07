8818d49cb88969aa252e9c23a7a8e596c84237ce
coompaadree <147946141+coompaadree@users.noreply.github.com>
2024-11-07 01:04:03 +0000

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
 document.querySelectorAll('.photo img').forEach(img => {
     img.addEventListener('click', () => {
-        console.log("Image clicked"); // Check if click event is firing
+        console.log("Image clicked");
         EXIF.getData(img, function() {
             let allMetaData = EXIF.getAllTags(this);
-            console.log("Metadata:", allMetaData); // Check the extracted metadata
+            console.log("Metadata:", allMetaData); 
         });
+    });
+});
+
+
+const images = document.querySelectorAll('.photo img');
+
+images.forEach(image => {
+    image.addEventListener('click', function() {
+        const imageName = image.src.split('/').pop();
+        window.location.href = `images.html?image=${imageName}`;
     });
 });
